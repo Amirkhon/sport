54771cb629882a7d92ebe4ca93182a259d88f228
Amirkhon Bahodurov <amirkhon.bahodurov@aciworldwide.com>
2018-01-27 15:46:48 +0300

    Minor refactoring

--- core/src/main/java/com/a31r/sport/coachassistant/core/model/repository/AthleteRepository.java
+++ core/src/main/java/com/a31r/sport/coachassistant/core/model/repository/AthleteRepository.java
@@ -1,7 +1,6 @@
 package com.a31r.sport.coachassistant.core.model.repository;
 
 import com.a31r.sport.coachassistant.core.model.Athlete;
-import com.a31r.sport.coachassistant.core.model.TrainingGroup;
 import com.a31r.sport.coachassistant.core.model.UserGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 
