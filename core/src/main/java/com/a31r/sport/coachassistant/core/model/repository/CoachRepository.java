3a725fa5fe0dc7a41ceb2bc515db9151fc82d392
Amirkhon Bahodurov <amirkhon.bahodurov@aciworldwide.com>
2018-01-28 09:16:53 +0300

    Unit tests were enhanced. Refactoring

--- core/src/main/java/com/a31r/sport/coachassistant/core/model/repository/CoachRepository.java
+++ core/src/main/java/com/a31r/sport/coachassistant/core/model/repository/CoachRepository.java
@@ -1,10 +1,16 @@
 package com.a31r.sport.coachassistant.core.model.repository;
 
 import com.a31r.sport.coachassistant.core.model.Coach;
+import com.a31r.sport.coachassistant.core.model.TrainingGroup;
+import com.a31r.sport.coachassistant.core.model.TrainingSession;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 /**
  * Created by bahodurova on 1/7/2018.
  */
 public interface CoachRepository extends JpaRepository<Coach, Long> {
+    List<Coach> findAllByTrainingGroupsContains(TrainingGroup group);
+    List<Coach> findAllByTrainingSessionsContains(TrainingSession session);
 }
